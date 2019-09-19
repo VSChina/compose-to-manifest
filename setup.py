68b21618b509a04667e331e9546e08dc406cd8ca
formulahendry <formulahendry@gmail.com>
2019-09-19 16:02:03 +0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iotedge-compose",
-    version="0.1.0",
+    version="0.1.1",
     author="iotedge",
     author_email="vsciet@outlook.com",
     description="Convert Docker Compose project to Azure IoT Edge deployment manifest",
